836c46613e64d735653ffa09bddebb54cfb26bc2
Boffir893 <kristoffer.borgersen@icloud.com>
2024-01-23 11:58:14 +0100

    ...

--- VÅR/algoritmer/algoritmer4.py
+++ VÅR/algoritmer/algoritmer4.py
@@ -1,4 +1,16 @@
 def n_høyeste(liste: list[int], n: int):
     høyeste_n = []
     for i in range(n):
-         
+
+
+
+
+def høyeste(liste: list[int]):
+    høyest = liste[0]
+    for tall in liste:
+        if tall > høyest:
+            høyest = tall
+    return høyest
+
+min_liste = høyeste
+print(høyeste(min_liste))
